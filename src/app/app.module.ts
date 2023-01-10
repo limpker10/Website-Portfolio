2fd8aae698700ce3037dbcaff861d7963bcbf423
José Miguel <jcanov@unsa.edu.pe>
2023-01-09 22:24:47 -0500

    Generate components initials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { WorksComponent } from './works/works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    WorksComponent
   ],
   imports: [
     BrowserModule,
